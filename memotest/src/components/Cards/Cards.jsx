e574fd1037ffcbe30869f8fc11070a9a5797eb96
Melina Iglesias <melinaiglesias03@gmail.com>
2024-11-23 14:07:51 -0300

    array de imagenes + shuffled

--- memotest/src/components/Cards/Cards.jsx
+++ memotest/src/components/Cards/Cards.jsx
@@ -1,23 +1,49 @@
-import React from 'react'
-import logoGot from '../../assets/images/got-icon.svg';
+import React, { useState, useEffect } from 'react';
 import arrow from '../../assets/images/arrow.svg';
 import book from '../../assets/images/book.svg';
 import wolf from '../../assets/images/wolf.svg';
+import knight from '../../assets/images/knight.svg';
 
 const Cards = () => {
+  const [shuffledCards, setShuffledCards] = useState([]);
 
-const images = [logoGot, arrow, book, wolf];
+  const images = [knight, arrow, book, wolf]; // paso las imagenes a un array
+  const arrayImages = images.flatMap((item) => [item, item]); // esto me lo duplica
 
-const arrayImages = images.flatMap(item => [item, item]);
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
 
-console.log(arrayImages); 
+  useEffect(() => {
+    const shuffled = shuffleArray([...arrayImages]); // aca me lo shufflea
+    setShuffledCards(
+      shuffled.map((image, i) => ({
+        id: i,
+        image,
+        flipped: false, 
+      }))
+    );
+  }, []);
 
-return (
-
-    <h1> a</h1>
-);
-
-
-}
+  return (
+    <div className="cards">
+      <h1>Memory Game</h1>
+      <div className="cards-container">
+        {shuffledCards.map((card) => (
+          <img
+            key={card.id}
+            src={card.image} 
+            alt={`Card ${card.id}`}
+            className={`card ${card.flipped ? 'flipped' : ''}`} 
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Cards;
