ea8f3b2eb40ec871d64c2befa1700fadd8796c9c
Melina Iglesias <melinaiglesias03@gmail.com>
2024-11-23 09:33:57 -0300

    creacion de componentes + array duplicados de imagenes

--- memotest/src/App.jsx
+++ memotest/src/App.jsx
@@ -1,21 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import LogoGot from './assets/images/got-icon.svg';
+import React from 'react'
+import logoGot from './assets/images/got-icon.svg';
+import arrow from './assets/images/arrow.svg';
+import book from './assets/images/book.svg';
+import wolf from './assets/images/wolf.svg';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const images = [logoGot, arrow, book, wolf];
+
+  const arrayImages = images.flatMap(item => [item, item]);
+
+  console.log(arrayImages); 
 
   return (
-    
-      <div>
-    
+    <div>
       <h1>Memory Game</h1>
-      </div>
-    
-
-  )
+      <p>¡Diviértete jugando con las imágenes!</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
